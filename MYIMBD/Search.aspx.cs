d7365f2fd3c75f9cbefcfc3a5d18d9c24f94af95
keithhetzel <keithh.8112@yahoo.com>
2015-12-31 03:57:00 -0600

    made a few changes to allow sql updates on all 3 buttons

--- MYIMBD/Search.aspx.cs
+++ MYIMBD/Search.aspx.cs
@@ -18,9 +18,6 @@ namespace MYIMBD
 {
     public partial class Search : Page
     {
-        int highfavid;
-        int highwantid;
-        int highnewid;
         public List<Models.FavoriteMovy> favs;
         public List<Models.WantToSeeMovy> wants;
         public List<Models.NewReleaseMovy> news;
@@ -28,7 +25,54 @@ namespace MYIMBD
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
 
+        public int getHighFav()
+        {
+            favs = new List<Models.FavoriteMovy>();
+            int favid = 0;
+            var q =
+                    from c in favs
+                    where c.P_Id < 100000
+                    select c;
+            foreach (Models.FavoriteMovy c in q)
+            {
+                favid += 1;
+            }
+            int highfavid = favid + 1;
+            return highfavid;
+        }
+
+        public int getHighWant()
+        {
+            wants = new List<Models.WantToSeeMovy>();
+            int wantid = 0;
+            var q =
+                    from c in wants
+                    where c.P_Id < 100000
+                    select c;
+            foreach (Models.WantToSeeMovy c in q)
+            {
+                wantid += 1;
+            }
+            int highwantid = wantid + 1;
+            return highwantid;
+        }
+
+        public int getHighNew()
+        {
+            news = new List<Models.NewReleaseMovy>();
+            int newid = 0;
+            var q =
+                    from c in news
+                    where c.P_Id < 100000
+                    select c;
+            foreach (Models.NewReleaseMovy c in q)
+            {
+                newid += 1;
+            }
+            int highnewid = newid + 1;
+            return highnewid;
         }
 
         public void SearchFor(object sender, EventArgs e)
@@ -65,8 +109,7 @@ namespace MYIMBD
             query += " VALUES (@P_Id, @Title, @Year, @Rated, @Released, @Runtime, @Genre, @Plot, @Director, @Writer, @IMDbRating, @PosterUrl, @Url)";
 
             SqlCommand myCommand = new SqlCommand(query, myConnection);
-            string highid = "SELECT MAX(P_Id) FROM FavoriteMovies;";
-            myCommand.Parameters.AddWithValue("@P_Id", highid +1);
+            myCommand.Parameters.AddWithValue("@P_Id", getHighFav());
             myCommand.Parameters.AddWithValue("@Title", Title1.Text);
             myCommand.Parameters.AddWithValue("@Year", Year.Text);
             myCommand.Parameters.AddWithValue("@Rated", Rated.Text);
@@ -84,12 +127,54 @@ namespace MYIMBD
 
         public void addToWants(object sender, EventArgs e)
         {
+            string connectionString = "workstation id = dccmoviestorage.mssql.somee.com; packet size = 4096; user id = donaldIMBD_SQLLogin_3; pwd = w7bfccaw49; data source = dccmoviestorage.mssql.somee.com; persist security info = False; initial catalog = dccmoviestorage";
+            SqlConnection myConnection = new SqlConnection(connectionString);
+            myConnection.Open();
 
+            string query = "INSERT INTO WantToSeeMovies (P_Id, Title, Year, Rated, Released, Runtime, Genre, Plot, Director, Writer, IMDbRating, PosterUrl, Url)";
+            query += " VALUES (@P_Id, @Title, @Year, @Rated, @Released, @Runtime, @Genre, @Plot, @Director, @Writer, @IMDbRating, @PosterUrl, @Url)";
+
+            SqlCommand myCommand = new SqlCommand(query, myConnection);
+            myCommand.Parameters.AddWithValue("@P_Id", getHighWant());
+            myCommand.Parameters.AddWithValue("@Title", Title1.Text);
+            myCommand.Parameters.AddWithValue("@Year", Year.Text);
+            myCommand.Parameters.AddWithValue("@Rated", Rated.Text);
+            myCommand.Parameters.AddWithValue("@Released", Released.Text);
+            myCommand.Parameters.AddWithValue("@Runtime", Runtime.Text);
+            myCommand.Parameters.AddWithValue("@Genre", Genre.Text);
+            myCommand.Parameters.AddWithValue("@Plot", Plot.Text);
+            myCommand.Parameters.AddWithValue("@Director", Director.Text);
+            myCommand.Parameters.AddWithValue("@Writer", Writer.Text);
+            myCommand.Parameters.AddWithValue("@IMDbRating", IMDbRating.Text);
+            myCommand.Parameters.AddWithValue("@PosterUrl", PosterUrl.Text);
+            myCommand.Parameters.AddWithValue("@Url", Url.Text);
+            myCommand.ExecuteNonQuery();
         }
 
         public void addToNew(object sender, EventArgs e)
         {
+            string connectionString = "workstation id = dccmoviestorage.mssql.somee.com; packet size = 4096; user id = donaldIMBD_SQLLogin_3; pwd = w7bfccaw49; data source = dccmoviestorage.mssql.somee.com; persist security info = False; initial catalog = dccmoviestorage";
+            SqlConnection myConnection = new SqlConnection(connectionString);
+            myConnection.Open();
 
+            string query = "INSERT INTO NewReleaseMovies (P_Id, Title, Year, Rated, Released, Runtime, Genre, Plot, Director, Writer, IMDbRating, PosterUrl, Url)";
+            query += " VALUES (@P_Id, @Title, @Year, @Rated, @Released, @Runtime, @Genre, @Plot, @Director, @Writer, @IMDbRating, @PosterUrl, @Url)";
+
+            SqlCommand myCommand = new SqlCommand(query, myConnection);
+            myCommand.Parameters.AddWithValue("@P_Id", getHighNew());
+            myCommand.Parameters.AddWithValue("@Title", Title1.Text);
+            myCommand.Parameters.AddWithValue("@Year", Year.Text);
+            myCommand.Parameters.AddWithValue("@Rated", Rated.Text);
+            myCommand.Parameters.AddWithValue("@Released", Released.Text);
+            myCommand.Parameters.AddWithValue("@Runtime", Runtime.Text);
+            myCommand.Parameters.AddWithValue("@Genre", Genre.Text);
+            myCommand.Parameters.AddWithValue("@Plot", Plot.Text);
+            myCommand.Parameters.AddWithValue("@Director", Director.Text);
+            myCommand.Parameters.AddWithValue("@Writer", Writer.Text);
+            myCommand.Parameters.AddWithValue("@IMDbRating", IMDbRating.Text);
+            myCommand.Parameters.AddWithValue("@PosterUrl", PosterUrl.Text);
+            myCommand.Parameters.AddWithValue("@Url", Url.Text);
+            myCommand.ExecuteNonQuery();
         }
 
     }
